cd0441d29c378790e5e17f2fc919876a74263db2
D-Vaibhav <vaibhav07nov@gmail.com>
2020-09-11 17:33:42 +0530

    gin/gorm api

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"try/Config"
+	"try/Models"
+	"try/Routes"
+
+	"github.com/jinzhu/gorm"
+)
+
+var err error
+
+func main() {
+	Config.DB, err = gorm.Open("mysql", Config.DbURL(Config.BuildDBConfig()))
+	if err != nil {
+		fmt.Println("Status:", err)
+	}
+	defer Config.DB.Close()
+	Config.DB.AutoMigrate(&Models.User{})
+	r := Routes.SetupRouter()
+	//running
+	r.Run()
+}
